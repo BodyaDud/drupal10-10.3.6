b2d3ecd50fab9af3e418983b317abf07c6d0ee6a
BodyaDud <dudchenkobohdan@gmail.com>
2024-10-17 00:30:42 +0300

    first commit

---
+++ web/themes/first/js/first.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * first behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.first = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
